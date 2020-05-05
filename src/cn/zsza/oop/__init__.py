8acdbdf5c0d4a0ac62b57a28a805307a67de21d2
zhangsong <zsza5466@sina.com>
2020-05-05 16:35:53 +0800

    删除一些code

--- src/cn/zsza/oop/__init__.py
+++ src/cn/zsza/oop/__init__.py
@@ -4,19 +4,6 @@
 # __author__ = 'zs'
 
 # 最简单的python类
-class ClassTest:
+class ClassTest(object):
     pass
 
-
-class Student(object):
-    # __init__：构造函数
-    # 两个下划线开头的变量是私有的
-    def __init__(self, name, score):
-        self.__name = name
-        self.__score = score
-
-    def set_name(self, name):
-        self.__name = name
-
-    def get_name(self):
-        return self.__name
