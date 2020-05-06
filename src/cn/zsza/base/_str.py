247b1e96e2395305a3e5c38ee8069fdc5cbf2b6b
zhangsong <zsza5466@sina.com>
2020-05-06 10:04:37 +0800

    提交字符串操作

---
+++ src/cn/zsza/base/_str.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'zs'
+# 2019/7/19 上午11:02
+
+
+import pyperclip
+
+# 逗号当做空格分割字符串
+print('hello nice,', 'I am in Beijing')
+
+print('I\'m ok!')
+# 字符串前面加r,对转义的字符串进行打印处理
+print(r'I\'m ok!')
+
+s1 = 'zs123bj'
+# 判断某个子串是否在字符串中
+print('12' in s1)
+
+l1 = ['a', 'b', 'c']
+# 用横线连接list中的元素
+print('-'.join(l1))
+
+# 字符串是否只包含字母
+print(s1.isalpha())
+
+# 字符串是否只包含字母和数字
+print(s1.isalnum())
+
+# 字符串是否只包含数字
+print(s1.isdecimal())
+
+s2 = 'Giraffe Academy'
+print('s2是否为全大写:', s2.isupper())
+
+# 复制到系统剪切板
+pyperclip.copy('something')
+
+
+# 字符串一些反转方法
+def reverse_str1(s):
+    return s[::-1]
+
+
+# 递归求字符串反转
+def reverse_str2(s):
+    if len(s) <= 1:
+        return s
+    return reverse_str2(s[1:]) + s[0:1]
+
+
+print(reverse_str1('abc'))
+print(reverse_str2('abcd'))
+
+# 换行字符串
+s3 = """asd
+df
+gh"""
+print(s3)
+
+# 求二进制数的十进制
+B = '1101'
+res = 0
+while B != '':
+    res = res * 2 + (ord(B[0]) - ord('0'))
+    B = B[1:]
+print(res)
